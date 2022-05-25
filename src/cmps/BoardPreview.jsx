dda685e8600885a4fc558b6025cecb5f781f81d5
Itamar Sahar <itamarsar81@gmail.com>
2022-05-25 16:19:40 +0300

    Boards and Boardspreview connect

--- src/cmps/BoardPreview.jsx
+++ src/cmps/BoardPreview.jsx
@@ -1,15 +1,20 @@
-
 import winter from '../assets/img/winter.png'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const style = {
-    'backgroundImage': winter,
+	backgroundImage: winter,
 }
 
-export const BoardPreview = ({board}) => {
-    ///<BoardHeader/>
-    return <article className='board-preview' style={{ 'backgroundColor': 'green' }}>
-        <h1>boardTitle</h1>
-        {/* <h1>board.title</h1> */}
-        <button className="star" >%</button>
-    </article>
+export const BoardPreview = ({ board }) => {
+	console.log(board)
+	///<BoardHeader/>
+	return (
+		<article className="board-preview" style={{ backgroundColor: 'green' }}>
+			<Link to={`boards/${board._id}`}>
+				<h1>{board.title}</h1>
+				<button className="star">%</button>
+			</Link>
+		</article>
+	)
 }
