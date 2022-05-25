555c08b3ef7d219dd2964206d8cd0a0098d05039
bar ivri <barevryaf@gmail.com>
2022-05-25 11:56:11 +0300

    startHome

--- src/cmps/header.jsx
+++ src/cmps/header.jsx
@@ -1,4 +1,10 @@
 
-export function AppHeader(){
-    return <header>Trello</header>
+export function AppHeader() {
+    return <header className="main-header flex space-between">
+        <h1 className="logo">Trello</h1>
+        <section className="nav-header flex">
+            <button className="login">Log in</button>
+            <button className="signup">Sign up</button>
+        </section>
+    </header>
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,14 @@
+import hero1 from '../assets/img/hero1.png'
+export function Home() {
+    return <main className='main-container'>
+        <section className='main-article flex justify-center align-center'>
+            <section className='main-article-content'>
+                <h1 className="home-text">Trello helps teams move work forward</h1>
+                <p>Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trello</p>
+            </section>
+            <img src={hero1} />
+        </section>
 
-export function Home(){
-    return 'welcome to trello' 
+
+    </main>
 }
