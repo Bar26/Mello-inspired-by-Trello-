2c09c2223a55c3a8e1064ef1cb3fae7288013a4f
Itamar Sahar <itamarsar81@gmail.com>
2022-05-25 12:03:27 +0300

    mistake

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
+import {utilService} from './services/util.service.js'
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
