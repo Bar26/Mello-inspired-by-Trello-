2bbda518b687d415dafc73618eb261cc91030f6f
bar ivri <barevryaf@gmail.com>
2022-05-25 14:18:52 +0300

    header-style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,11 @@ import hero1 from '../assets/img/hero1.png'
 export function Home() {
     return <main className='main-container'>
         <section className='main-article flex justify-center align-center'>
-            <section className='main-article-content'>
-                <h1 className="home-text">Trello helps teams move work forward</h1>
-                <p>Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trello</p>
+            <section className='content'>
+                <h1 className="title">Trello helps teams move work forward</h1>
+                <p className='trello-prev'>Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trello</p>
             </section>
-            <img src={hero1} />
+            <img className='hero1' src={hero1} />
         </section>
-
-
     </main>
 }
