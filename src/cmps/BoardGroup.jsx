32cc2bb5d97b8d07016c6482bcc6cb1a1466eed6
Noam <noambar2845@gmail.com>
2022-05-25 16:21:00 +0300

    Sepereted

---
+++ src/cmps/BoardGroup.jsx
@@ -0,0 +1,19 @@
+
+
+
+export const BoardGroup = () => {
+
+
+
+    return <article className={`group`}>
+        <div className='group-title'>
+            <h1>To Do:</h1>
+            {/* todo: Options */}
+        </div>
+        <div className='card-taskes'>
+            <div>Task1</div>
+            <div>Task2</div>
+            <div>Task3</div>
+        </div>
+    </article>
+}
--- src/cmps/BoardPreview.jsx
+++ src/cmps/BoardPreview.jsx
@@ -5,11 +5,16 @@ const style = {
     'backgroundImage': winter,
 }
 
-export const BoardPreview = ({board}) => {
+export const BoardPreview = ({ board }) => {
     ///<BoardHeader/>
     return <article className='board-preview' style={{ 'backgroundColor': 'green' }}>
         <h1>boardTitle</h1>
         {/* <h1>board.title</h1> */}
-        <button className="star" >%</button>
+        {/* <button className="star" >%</button> */}
+        <label className="star">
+            <button></button>
+                <i class="fa-regular fa-star"></i>
+            {/* <i className="fa-solid fa-copy"></i> */}
+        </label>
     </article>
 }
